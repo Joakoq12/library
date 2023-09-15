49f992cc10f181cd00a4d2005e80cc3ecd288fe8
Joakoq12 <fullestnewyork@gmail.com>
2023-09-15 13:49:04 -0400

    New Project

---
+++ src/pages/create.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+export default function Create() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [cover, setCover] = useState("");
+  const [intro, setIntro] = useState("");
+  const [completed, setCompleted] = useState(false);
+  const [review, setReview] = useState("");
+
+
+  function handleChange(e) {
+    const name = e.target.name; 
+    const value = e.target.value; 
+
+    switch(name) {
+        case 'title' : 
+        setTitle(value);
+        break;
+
+
+       default:
+
+    }
+
+
+  }
+
+  return (
+    <div>
+      <form>
+        <div>
+        <div>Title</div>
+        <input
+         type="text"
+         name="title"
+         onChange={handleChange}
+        value={title}
+         />
+        </div>
+
+
+        <div>
+        <div>Author</div>
+        <input
+         type="text"
+         name="author"
+         onChange={handleChange}
+        value={author}
+         />
+        </div>
+
+
+
+        <div>
+        <div>TITLE</div>
+        <input
+         type="text"
+         name="title"
+         onChange={handleChange}
+        value={title}
+         />
+        </div> 
+      </form>
+      
+    </div>
+  );
+}
